74aaaff6170b8da73e51156ce41094b6c0b0b871
Khanh Ton Nu Ngoc <s3932105@rmit.edu.vn>
2022-07-27 15:43:47 +0700

    new one

--- week5/password.js
+++ week5/password.js
@@ -18,3 +18,20 @@ else {
 }
 
 // check the existent of an upper case letter
+let foundUpperCase = false;
+for (let i = 0; i < pass.length; i++) {
+    let c = pass.charAt(i);
+    if ('A' <= c && c <= 'Z') {
+        foundUpperCase = true;
+        break;
+    }
+}
+if (foundUpperCase) {
+    res.innerText= 'Password is OK';
+    res.classList.add('status-success');
+    res.classList.remove('status-error');
+} else {
+    res.innerText= 'No upper case letter can be found';
+    res.classList.add('status-error');
+    res.classList.remove('status-success');
+}
